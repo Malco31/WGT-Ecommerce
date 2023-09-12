a348fdf5190fe60a87dda268ff13469586571bf0
Malco31 <johnson.malcolm2003@gmail.com>
2023-09-11 20:10:09 -0500

    First Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    var menu = document.getElementById("MenuItems");
+    if (menu.style.display === "none" || menu.style.display === "") {
+        menu.style.display = "flex";
+    } else {
+        menu.style.display = "none";
+    }
+}
